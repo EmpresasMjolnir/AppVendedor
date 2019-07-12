8d07b751ab82578fe664cb6a819e73e7059a9271
EmpresasMjolnir <empresasmjolnir@gmail.com>
2019-07-11 21:39:38 -0300

    2

--- src/app/pages/cadastrodoproduto/cadastrodoproduto.page.ts
+++ src/app/pages/cadastrodoproduto/cadastrodoproduto.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from 'src/app/model/produto';
 import { DadosProdutoService } from 'src/app/services/dados-produto.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-cadastrodoproduto',
@@ -9,21 +12,45 @@ import { DadosProdutoService } from 'src/app/services/dados-produto.service';
 })
 export class CadastrodoprodutoPage implements OnInit {
   
-  private produto:Produto = new Produto;
+  public produto: Produto;
 
-  constructor(private DadosProduto: DadosProdutoService) { }
+  constructor(private DadosProduto: DadosProdutoService, private alertController: AlertController,
+    public afAuth: AngularFireAuth,
+    public router: Router) {}
 
   ngOnInit() {
+      this.produto = new Produto
   }
-  async registraProduto(){
-    await this.DadosProduto.addDados(this.produto).subscribe(
-      result=>{
-          console.log('gravado com sucesso');
-      },
-        error => {
-          console.error();
-      }
-    )
+  onSubmit(form) {
+    this.DadosProduto.save(this.produto)
+      .then(
+        res => {
+          console.log("Cadastrado");
+          this.presentAlert("Aviso!", "Produto cadastrado.");
+          this.produto = new Produto
+        }
+        ,
+        err => {
+          console.log("Epá! Não foi cadastrado!" + err);
+          this.presentAlert("Erro!", "Epá! Não foi cadastrado!");
+        }
+      ).catch(
+        erros => {
+          console.log("Erro ao conectar no sistema! " + erros);
+          this.presentAlert("Erro!", "Erro ao conectar no sistema!");
+        }
+      )
+  }
+
+  //Alerts -------------------------------
+  async presentAlert(titulo: string, texto: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      // subHeader: 'Subtitle',
+      message: texto,
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 
 }
--- src/app/services/dados-produto.service.ts
+++ src/app/services/dados-produto.service.ts
@@ -1,49 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Produto } from '../model/produto';
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { AngularFireDatabase } from '@angular/fire/database';
 
-const URL_API = `http://10.69.136.129:3000/cadastroProduto`
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-type': 'application/json;charset=utf-8'
-  })
-}
 @Injectable({
   providedIn: 'root'
 })
 export class DadosProdutoService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private bd: AngularFireDatabase) { }
 
-  getDados(param:string = ""):Observable<Produto> {
-      return this.http.get<Produto>(`${URL_API}${param}`,httpOptions).pipe(
-      catchError(this.handleError<any>('Falha ao recuperar os dados'))
-    );
-  }
-
-  addDados(dados:Produto):Observable<Produto>{
-      return this.http.post(URL_API, dados, httpOptions).pipe(
-      catchError(this.handleError<any>('Falha ao registra a avaliação'))
-    )
-  }
-
-  updateDados(dados:Produto):Observable<Produto>{
-      return this.http.put(`${URL_API}/${dados.id}`, dados, httpOptions).pipe(
-      catchError(this.handleError<any>('Falha ao atualizar a avaliação'))
-    )
-  }
-
-  //método privado para exibir o erro
-  private handleError<T>(Operator = 'operation', result?: T){
-    return (error: any):Observable<T> => {
-      console.error(error); //log do erro na console
-
-      //mantem o app rodando por ter retornado o obj vazio
-      return of(result as T);
-    };
+  save(Produto: Produto) {
+    return this.bd.list("produto").push(Produto);
   }
 
 }
